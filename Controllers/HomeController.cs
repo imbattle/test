38105e41e385ca47a6878b4c32d54508492965ee
battle <imbattle@gmail.com>
2020-05-19 11:16:52 +0800

    test for prj

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using prjModelValidate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjModelValidate.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbStudentEntities db = new dbStudentEntities();
+        
+        public ActionResult Index()
+        {
+            var students = db.tStudent.ToList();
+            return View(students);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tStudent stu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tStudent.Add(stu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(stu);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var stu = db.tStudent.Where(m => m.fStuId == id).FirstOrDefault();
+            db.tStudent.Remove(stu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ModelMetaData/tStudent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace prjModelValidate.Models.ModelMetaData
+{
+    [MetadataType(typeof(tStudentMetaData))]
+    public class tStudent
+    {
+        private class tStudentMetaData
+        {
+            [DisplayName("學號")]
+            [Required(ErrorMessage = "學號不可空白！！")]
+            [StringLength(7, MinimumLength = 2, ErrorMessage = "學號必須是2-7個字元！！")]
+            public string fStuId { get; set; }
+            [DisplayName("姓名")]
+            [Required(ErrorMessage = "姓名不可空白！！")]
+            public string fName { get; set; }
+            [DisplayName("信箱")]
+            [EmailAddress(ErrorMessage = "E-Mail 格式有誤！！")]
+            public string fEmail { get; set; }
+            [DisplayName("成績")]
+            [Range(0, 100, ErrorMessage = "分數必須是0-100！！")]
+            public Nullable<int> fScore { get; set; }
+        }
+    }
+}
